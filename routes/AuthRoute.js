e3059f6fb42bca37122279d49baa6ba4457c3331
root <rahmatwahyubudiyanto@gmail.com>
2022-10-26 09:54:59 +0700

    ketinggalan valid char

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import * as controller from './../controllers/AuthController.js'
 import EmptyUserValidator from './../middlewares/EmptyUserValidator.js'
+import ValidCharacter from './../middlewares/ValidCharacter.js'
 
 const router = Router()
 
 router.get('/get-user', controller.getUser)
-router.post('/register', controller.register)
+router.post('/register', ValidCharacter, controller.register)
 router.post('/login', EmptyUserValidator, controller.login)
 router.post('/verify-otp', EmptyUserValidator, controller.verifyOtp)
 
