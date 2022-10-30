80079bc0fed8c310888b2b2f27210472024479e6
root <rahmatwahyubudiyanto@gmail.com>
2022-10-30 19:33:47 +0700

    validate maximum character 13 at register

--- middlewares/CharacterValidator.js
+++ middlewares/CharacterValidator.js
@@ -13,6 +13,10 @@ const validate = async(req, res, next) => {
             return response(res, 422, false, 'Name must be at least 3 characters of letter!')
         }
 
+        if(name.length > 13){
+            return response(res, 422, false, 'Name must be 13 characters of maximum letter!')
+        }
+
         next()
 
     } catch (err) {
--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 import * as controller from './../controllers/AuthController.js'
 import EmptyUserValidator from './../middlewares/EmptyUserValidator.js'
-import ValidCharacter from './../middlewares/ValidCharacter.js'
+import CharacterValidator from './../middlewares/CharacterValidator.js'
 import formatTelpValidator from './../middlewares/formatTelpValidator.js'
 
 const router = Router()
 
 router.get('/get-user', controller.getUser)
-router.post('/register', formatTelpValidator, ValidCharacter, controller.register)
+router.post('/register', formatTelpValidator, CharacterValidator, controller.register)
 router.post('/login', formatTelpValidator, EmptyUserValidator, controller.login)
 router.post('/verify-otp', formatTelpValidator, EmptyUserValidator, controller.verifyOtp)
 
