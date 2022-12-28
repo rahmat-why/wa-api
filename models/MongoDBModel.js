fadfbe5cca8aa9d6781ceaf138a57f97c1419945
yangTauTauAjah <tesakun6464@gmail.com>
2022-12-28 20:36:22 +0700

    Changed _id datatype from ObjectId to String

--- models/MongoDBModel.js
+++ models/MongoDBModel.js
@@ -1,16 +1,19 @@
 import mongoose, { Schema } from 'mongoose'
 
 export const States = mongoose.model('states', new Schema({
+  _id: String,
   name: String,
   message: String
 }))
 
 export const ChatFlows = mongoose.model('chatflows', new Schema({
+  _id: String,
   name: String,
   description: String
 }))
 
-export const Journies = mongoose.model('journies', new Schema({
+export const Journeys = mongoose.model('journeys', new Schema({
+  _id: String,
   chatflow_id: mongoose.Types.ObjectId,
   current_state: mongoose.Types.ObjectId,
   next_state: mongoose.Types.ObjectId,
